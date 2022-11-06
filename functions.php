7713384efca583fa04adafb5a549902e256cc265
YingHing <yinghing.lam@gmail.com>
2022-11-06 14:59:16 +0100

    fixed article images
    ty martijn

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 $folder = '/functions';
-$roots_includes = ['/menus.php', '/themesupport.php'];
+$roots_includes = ['/menus.php', '/themesupport.php', '/register-styles.php'];
 
 foreach ($roots_includes as $file) {
     $value = $folder . $file;
@@ -23,16 +23,9 @@ function studiolamby_register_styles(){
     wp_enqueue_style('studiolamby-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
 
-add_action('wp_enqueue_scripts', 'studiolamby_register_styles');
 
-function studiolamby_register_scripts(){
 
-    wp_enqueue_script('studiolamby-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
-    wp_enqueue_script('studiolamby-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
-    wp_enqueue_script('studiolamby-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
-    wp_enqueue_script('studiolamby-mainjs', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
 
-}
 
 add_action('wp_enqueue_scripts', 'studiolamby_register_scripts');
 
--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,58 +1,34 @@
 <div class="container-fluid">
     <header class="content-header">
-        <?php 
-            // $args = array( 
-            //     'post_type' => 'attachment', 
-            //     'post_mime_type' => 'image',
-            //     'post_status' => null, 
-            //     'post_parent' => $post->ID 
-            // );
-
-            $args = [
-                'post_type' => 'attachment',
-                'post_mime_type' => 'image',
-                'orderby' => 'post_date',
-                'order' => 'desc',
-                'posts_per_page' => '10',
-                // 'post_parent' => $post->ID 
-            ];
-            
+        <?php
+            $args = get_post_gallery_images(get_the_ID());
             $oddLoop = true;
 
-            function post($args, $oddLoop){
-                $attachments = get_posts( $args );
-                // var_dump($attachments);
-                if ($attachments) {
-                    foreach ( $attachments as $post) {
-
-                        if ($oddLoop =! $oddLoop){
-                                
-                            $fullsize = wp_get_attachment_image_src($post->ID);
-
-                            unset($fullsize[1]);
-                            unset($fullsize[2]);
-                            
-                            $implodeArray = implode(" ",$fullsize);
-                            echo "<img src='{$implodeArray}' class='mb-4'/>";
+            function post($args, $oddLoop)
+            {
+                if (!empty($args)) {
+                    foreach ($args as $image) {
+                        if ($oddLoop = !$oddLoop) {
+                            echo "<img src='{$image}' class='mt-4'/>";
                         }
                     }
-                } 
-            }   
+                }
+            }
         ?>
             <header class="content-header">
                 <div class="row">
 
-                    <div class="col-4 post_text">
+                    <div class="col-sm post_text">
                         <h1> <?php echo(the_title()); ?> </h1>
                         <p> <?php echo wp_strip_all_tags( get_the_content() ); ?> </p>
                     </div>
 
-                    <div class="col-4">
-                        <?php post($args, $oddLoop = false); ?>
+                    <div class="col-sm">
+                        <?php post($args, $oddLoop = true); ?>
                     </div>
 
-                    <div class="col-4">
-                        <?php post($args, $oddLoop = true); ?>
+                    <div class="col-sm">
+                        <?php post($args, $oddLoop = false); ?>
                     </div>
 
                 </div>
