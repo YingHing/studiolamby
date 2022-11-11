f5fefe8f4c67881da014d369868ffdf8deb82491
YingHing <yinghing.lam@gmail.com>
2022-11-11 22:30:55 +0100

    contact setup

---
+++ template-parts/content-contact.php
@@ -0,0 +1,7 @@
+<div class="container-fluid px-4 size-title-mobile fixed-bottom">
+        <?php 
+            $current_page = get_queried_object();
+            $content      = apply_filters( 'the_content', $current_page->post_content );
+            echo $content;
+        ?>
+</div>
