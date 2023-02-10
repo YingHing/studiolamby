d818029cf7c83ef3770b3a15e06cdb668d864dc8
YingHing <yinghing.lam@gmail.com>
2023-02-10 16:35:40 +0100

    index styling

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,27 +1,29 @@
 <div class="row index-hover" style="transform: rotate(0); height:90px;">
     <hr class="mx-auto" style="width:110%;"/>
         <div class="col px-4">
-            <h1>
+            <h1 class="responsive-index">
                 <?php
                     the_date('Y');
                 ?>
             </h1>
         </div>
         <div class="col px-4">
-            <h1 class="mobile-text-end">
+            <h1 class="mobile-text-end responsive-index">
                 <?php
                     the_title();
                 ?>
             </h1>
         </div>
-        <div class="col px-4 d-none d-mg-none d-lg-block">
-            <h1 class="text-end">
+        <div class="col-7 px-4 d-none d-mg-none d-lg-block">
+            <h1 class="text-end responsive-index">
                 <?php
-                    $posttags = get_the_tags();
-                    if ($posttags) {
-                        foreach($posttags as $tag) {
-                            echo $tag->name . ' '; 
+                    $post_tags = get_the_tags();
+                    if ($post_tags) {
+                        $tag_list = array();
+                        foreach ($post_tags as $tag) {
+                        $tag_list[] = $tag->name;
                         }
+                        echo implode(" + ", $tag_list);
                     }
                 ?>
             </h1>
